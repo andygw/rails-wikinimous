5794a3f0d63862071a3f43e9292779a79feac204
Andrew Wever <andrew.wever@gmail.com>
2022-11-08 15:40:56 -0500

    Wikinimous clone

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # All routes:
+  resources :articles
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Subset of routes:
+  # resources :articles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
