1c2ca12958b2193fb7f4b042ca924b292f4e82bc
Andrew Wever <andrew.wever@gmail.com>
2022-11-08 14:04:07 -0500

    model and seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  title = Faker::Book.title
+  content = Faker::Quote.jack_handey
+  Article.create(title: title, content: content)
+end
